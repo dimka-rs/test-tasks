d8c5657a2ed67a40ebda343f8f4f53b20e054dd8
Dmitry K <dimka.rs@gmail.com>
2020-10-08 16:42:27 +0300

    uart: improve test 1

--- uart/test.py
+++ uart/test.py
@@ -23,9 +23,12 @@ def test1(pin, pout):
     for i in range(buf_size - 1):
         os.write(pin, b'1')
 
+    ## wait for program to process
+    time.sleep(1)
+
     ## try read back, must get nothing
     if try_read(pout) != 0:
-        print("first read has data")
+        print("First read has data, but must return nothing")
         return 1
 
     ## write last byte to make full buffer
@@ -40,7 +43,7 @@ def test1(pin, pout):
         cnt += 1
 
     if cnt != buf_size:
-        print("Secont read failed, cnt = ", cnt, "must be ", buf_size)
+        print("Second read ", cnt, " does not match buffer size ", buf_size)
         return 1
 
     return 0
@@ -51,7 +54,7 @@ if __name__ == "__main__":
         print("Usage %s pipe_in pipe_out" % (sys.argv[0]))
         exit(1)
 
-    pin = os.open(sys.argv[1], os.O_WRONLY | os.O_NONBLOCK)
+    pin =  os.open(sys.argv[1], os.O_WRONLY | os.O_NONBLOCK)
     pout = os.open(sys.argv[2], os.O_RDONLY | os.O_NONBLOCK)
 
     ret = test1(pin, pout)
